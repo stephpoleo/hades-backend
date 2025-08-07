b13e09eed62e7b834d626a86d8eb5ef3c79abf8c
José Hernández <joser_9694@hotmail.com>
2025-08-07 14:48:27 -0600

    Update

--- hades_app/models.py
+++ hades_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class EDS(models.Model):
+    id_eds_pk = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=65, null=True, blank=True)
+    plaza = models.CharField(max_length=20, null=True, blank=True)
+    state = models.CharField(max_length=20, null=True, blank=True)
+    municipality = models.CharField(max_length=35, null=True, blank=True)
+    zip_code = models.CharField(max_length=10, null=True, blank=True)
+    plaza_status = models.BooleanField(null=True, blank=True)
+    long_eds = models.DecimalField(max_digits=20, decimal_places=10, null=True, blank=True)
+    latit_eds = models.DecimalField(max_digits=20, decimal_places=10, null=True, blank=True)
+
+    class Meta:
+        db_table = 'EDS'
--- hades_app/views.py
+++ hades_app/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from .migrations.models import EDS
+from rest_framework import serializers
 
-# Create your views here.
+class EDSSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EDS
+        fields = '__all__'
+
+class EDSView(APIView):
+    """CRUD para EDS"""
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            id = request.query_params.get("id")
+            if id is not None:
+                eds = EDS.objects.get(pk=id)
+                serializer = EDSSerializer(eds)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            if e.args and "matching query does not exist" in str(e):
+                return Response(
+                    {"message": "Wrong EDS id or does not exist"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            else:
+                eds = EDS.objects.all()
+                serializer = EDSSerializer(eds, many=True)
+                if eds.exists():
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response(
+                        {"message": "EDS not found"},
+                        status=status.HTTP_200_OK,
+                    )
+
+    def post(self, request):
+        serializer = EDSSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "New EDS created", "data": serializer.data},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        id = request.query_params.get("id")
+        try:
+            eds_instance = EDS.objects.get(pk=id)
+        except Exception:
+            return Response(
+                {"message": "Wrong EDS id or does not exist"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = EDSSerializer(eds_instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "EDS updated", "data": serializer.data},
+                status=status.HTTP_200_OK,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        id = request.query_params.get("id")
+        try:
+            eds_instance = EDS.objects.get(pk=id)
+        except Exception:
+            return Response(
+                {"message": "Wrong EDS id or does not exist"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        eds_instance.delete()
+        return Response(
+            {"message": f"EDS {id} was deleted"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
