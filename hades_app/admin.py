d5d05d633dc398aaea664fa134f3b55824cb6b46
stephpoleo <stephaniepoleo@hotmail.com>
2025-08-13 19:01:45 -0600

    Needed configurations

--- hades_app/admin.py
+++ hades_app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Users
 
 # Register your models here.
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'role_name', 'usr_status']
+    list_filter = ['usr_status', 'id_role_fk']
+    search_fields = ['name', 'email']
+    ordering = ['name']
+    
+    fieldsets = (
+        ('Información Personal', {
+            'fields': ('name', 'email', 'password')
+        }),
+        ('Estado y Permisos', {
+            'fields': ('usr_status', 'id_role_fk')
+        }),
+        ('Asignaciones Futuras', {
+            'fields': ('id_eds_fk', 'id_work_area_fk')
+        }),
+    )
+    
+    def save_model(self, request, obj, form, change):
+        # Si es un nuevo usuario o se cambió la contraseña, encriptarla
+        if not change or 'password' in form.changed_data:
+            if obj.password and not obj.password.startswith('pbkdf2_'):
+                obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
